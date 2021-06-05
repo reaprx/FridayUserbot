aaaedf5a096489256fcb42a7cd80a9ca82d53ca7
Hasalanka Seneviratne <seneviratnehasalanka@gmail.com>
2021-06-05 21:27:11 +0530

    Update main_assistant.py

--- assistant/main_assistant.py
+++ assistant/main_assistant.py
@@ -39,15 +39,15 @@ async def start(client, message):
     bot_username = starkbot.username
     firstname = message.from_user.first_name
     user_id = message.from_user.id
-    starttext = f"`Hello, {firstname} ! Nice To Meet You, Well I Am {bot_name}, An Powerfull Assistant Bot To Talk And Do Many Things For My Master!`. \n\nPowered By [Friday Userbot](t.me/FridayOT)"
+    starttext = f"`Hello, {firstname} ! Nice To Meet You, Well I Am {bot_name}, An Powerfull Assistant Bot To Talk And Do Many Things For My Master!`. \n\nPowered By [ReaperX](t.me/reapr_x)"
     mypic = Config.ASSISTANT_START_PIC
     if user_id not in all_user_s:
         await client.send_photo(
             message.chat.id,
             mypic,
             starttext,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Help Me ❓", url="t.me/Fridayot")]]
+            reply_markup=InlineKeyboardMarkup(u
+                [[InlineKeyboardButton("Help Me ❓", url="t.me/reapr_x")]]
             ),
         )
         kok = await check_user(user_id)
